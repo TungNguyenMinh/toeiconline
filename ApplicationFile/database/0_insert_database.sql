e35f721148e210071338808e823b0e3a9886f1ec
=lethigam150594 <=tungnguyenminh271294@gmail.com>
2020-01-15 22:46:13 +0700

    create login page and int resource bundle

---
+++ ApplicationFile/database/0_insert_database.sql
@@ -0,0 +1,3 @@
+USE toeiconline;
+INSERT INTO user(name, password, fullname, createddate, roleid) VALUE ('admin', '123456', 'admin', CURRENT_TIMESTAMP, 1);
+INSERT INTO user(name, password, fullname, createddate, roleid) VALUE ('minhtung', '123456', 'minhtung', CURRENT_TIMESTAMP, 1);
