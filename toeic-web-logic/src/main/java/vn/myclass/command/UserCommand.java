e30296afb82bdcc46c90bcef84e3004dc0751b33
lethigam150594 <tungnguyenminh271294@gmail.com>
2020-01-26 18:03:23 +0700

    init common bean utils

--- toeic-web-logic/src/main/java/vn/myclass/command/UserCommand.java
+++ toeic-web-logic/src/main/java/vn/myclass/command/UserCommand.java
@@ -4,7 +4,16 @@ import vn.myclass.core.command.AbstractCommand;
 import vn.myclass.core.dto.UserDTO;
 
 public class UserCommand extends AbstractCommand<UserDTO> {
+    private String confirmPassword;
     public UserCommand() {
         this.pojo = new UserDTO();
+    }
+
+    public String getConfirmPassword() {
+        return confirmPassword;
+    }
+
+    public void setConfirmPassword(String confirmPassword) {
+        this.confirmPassword = confirmPassword;
     }
 }
--- toeic-web-logic/src/main/java/vn/myclass/controller/admin/LoginController.java
+++ toeic-web-logic/src/main/java/vn/myclass/controller/admin/LoginController.java
@@ -1,5 +1,10 @@
 package vn.myclass.controller.admin;
 
+import org.apache.log4j.Logger;
+import vn.myclass.command.UserCommand;
+import vn.myclass.core.dto.UserDTO;
+import vn.myclass.core.utils.FormUtil;
+
 import javax.servlet.RequestDispatcher;
 import javax.servlet.ServletException;
 import javax.servlet.annotation.WebServlet;
@@ -10,6 +15,7 @@ import java.io.IOException;
 
 @WebServlet("/login.html")
 public class LoginController extends HttpServlet {
+    private final Logger log = Logger.getLogger(this.getClass());
     @Override
     protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
         RequestDispatcher rd = request.getRequestDispatcher("/views/web/login.jsp");
@@ -18,6 +24,9 @@ public class LoginController extends HttpServlet {
 
     @Override
     protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
-
+        /*UserCommand command = FormUtil.populate(UserCommand.class, request);
+        UserDTO pojo = command.getPojo();*/
+        RequestDispatcher rd = request.getRequestDispatcher("/views/web/login.jsp");
+        rd.forward(request, response);
     }
 }
