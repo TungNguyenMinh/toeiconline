b2222f117dc100c2770037192feea0f08b40d2d2
LANANH <tungnguyenminh271294@gmail.com>
2020-02-13 07:41:38 +0700

    create page edit v2

--- toeic-core-common/src/main/java/vn/myclass/core/common/utils/UploadUtil.java
+++ toeic-core-common/src/main/java/vn/myclass/core/common/utils/UploadUtil.java
@@ -1,4 +1,12 @@
 package vn.myclass.core.common.utils;
 
+import javax.servlet.http.HttpServletRequest;
+
 public class UploadUtil {
+    private final int maxMemorySize = 1024 * 1024 * 3; //3M
+    private final int maxRequestSize = 1024 * 1024 * 50; //50M
+
+    public void writeOrUpdateFile(HttpServletRequest request) {
+
+    }
 }
