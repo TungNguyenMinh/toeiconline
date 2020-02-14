f7d8d52cfe7d2e9245e1f7a0c75b5e3a51c337e6
LANANH <tungnguyenminh271294@gmail.com>
2020-02-14 07:36:35 +0700

    uploadutil v2

--- toeic-core-common/src/main/java/vn/myclass/core/common/utils/UploadUtil.java
+++ toeic-core-common/src/main/java/vn/myclass/core/common/utils/UploadUtil.java
@@ -1,5 +1,7 @@
 package vn.myclass.core.common.utils;
 
+import org.apache.commons.fileupload.servlet.ServletFileUpload;
+
 import javax.servlet.http.HttpServletRequest;
 
 public class UploadUtil {
@@ -7,6 +9,7 @@ public class UploadUtil {
     private final int maxRequestSize = 1024 * 1024 * 50; //50M
 
     public void writeOrUpdateFile(HttpServletRequest request) {
-
+        // Check that we have a file upload request
+        boolean isMultipart = ServletFileUpload.isMultipartContent(request);
     }
 }
